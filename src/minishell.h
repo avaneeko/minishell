a66f152e66d641a40d54d6244d7a1f1320a2f09c
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-07-29 15:17:46 +0300

    Define token types.

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 18:41:55 by losypenk          #+#    #+#             */
-/*   Updated: 2025/07/26 16:26:02 by losypenk         ###   ########.fr       */
+/*   Updated: 2025/07/29 15:16:12 by losypenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,33 @@
 # include <readline/history.h>
 
 # include "utils.h"
+
+enum e_token_type
+{
+	TOKEN_UNDEFINED,
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_INPUT,
+	TOKEN_REDIRECT_OUTPUT,
+	TOKEN_REDIRECT_OUTPUT_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_VARIABLE
+};
+
+//
+//	Defines a token.
+//!	.token might be missing from certain .type tokens.
+//
+typedef struct s_token
+{
+	unsigned int	type;		// Token type from e_token_type
+	char			token[];	// Token's contents.
+}	t_token;
+
+typedef struct s_token_list
+{
+	t_token			*tok;	// Token ptr array.
+	unsigned int	len;	// It's length.
+}	t_token_list;
 
 #endif
