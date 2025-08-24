5ff9e6d4cbcf971a2b465f65faa9d693d795991a
unknown <danielinkmou@gmail.com>
2025-08-24 16:19:35 +0300

    minor syntax changes and wrap up return value in brackets

--- src/Execution/Builtins/builtins.c
+++ src/Execution/Builtins/builtins.c
@@ -31,19 +31,19 @@ int	is_builtin(const char *cmd)
 
 int	exec_builtin(char **argv, t_env *env)
 {
-	if (streq(argv[0], "cd"))
+	if (streq(argv, "cd"))
 		return (builtin_cd(argv, env));
 	 if (streq(argv, "echo"))
-        return builtin_echo(argv);
+        return (builtin_echo(argv));
     if (streq(argv, "pwd"))
-        return builtin_pwd();
+        return (builtin_pwd());
     if (streq(argv, "export"))
-        return builtin_export(argv, env);
+        return (builtin_export(argv, env));
     if (streq(argv, "unset"))
-        return builtin_unset(argv, env);
+        return (builtin_unset(argv, env));
     if (streq(argv, "env"))
-        return builtin_env(env);
+        return (builtin_env(env));
     if (streq(argv, "exit"))
-        return builtin_exit(argv);
+        return (builtin_exit(argv));
     return (0);
 }
