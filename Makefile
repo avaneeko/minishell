e2236d4b3f17b442575390793259c5fa93e99d2c
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-09-12 14:52:28 +0300

    draft: Heredoc: token handler, prompt and expansion.

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/24 17:33:26 by losypenk          #+#    #+#              #
-#    Updated: 2025/09/12 12:12:11 by losypenk         ###   ########.fr        #
+#    Updated: 2025/09/12 14:49:42 by losypenk         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ NAME := minishell
 SRC := src/main.c src/utils.c src/utils2.c src/utils3.c src/token.c \
 	src/token_list.c src/token_list2.c src/tokenizer.c src/app_create.c \
 	src/app_destroy.c src/env.c src/expansion.c src/astr.c src/token_resplit.c \
-	src/heredoc.c
+	src/heredoc.c src/expansion_utils.c
 OBJ := $(SRC:src/%.c=obj/%.o)
 
 CC := cc
--- src/heredoc.c
+++ src/heredoc.c
@@ -43,10 +43,10 @@ char const	*get_heredoc_filename(int unsigned idx)
 }
 
 // Try to open a new heredoc. Opens a file to be used for heredoc.
-int	try_open_heredoc(t_app *app)
+// idx - heredoc fd.
+int	try_open_heredoc(t_app *app, int /*out*/ *idx)
 {
 	int unsigned	i;
-
 
 	i = ~0;
 	while (++i < 16)
@@ -54,23 +54,71 @@ int	try_open_heredoc(t_app *app)
 		if (app->heredocs[i] == -1)
 			break;
 	}
-	if (i == 16)
+	if (i >= 16)
 	{
 		write(2, "minishell: maximum here-document count exceeded", 47);
 		app_destroy(app);
 		exit(1);
 	}
+	app->heredocs[i] = open(get_heredoc_filename(i), O_RDWR | O_CREAT | O_EXCL,
+		0600);
+	if (app->heredocs[i] == -1)
+		return (0);
+	*idx = app->heredocs[i];
+	// can't unlink here cuz the files needs to be re-open later.
+	return (1);
+}
+
+//* will close *fd on failure.
+int	get_heredoc_input(t_app *app, int *fd, char const *heredoc_end, int exp)
+{
+	char	*input;
+
+	if (isatty(STDIN_FILENO))
+	{
+		// TODO: Signals for readline here.
+		input = readline("> ");
+		if (!input)
+		{
+			// CTRL+D
+		}
+		else if (streq(input, heredoc_end))
+		{
+			// Found end of heredoc.
+
+		}
+		else if (input && exp)
+		{
+
+		}
+	}
+}
+
+int expand_heredoc(t_env const *env, int should_expand)
+{
+	t_astr	a;
+
+	if (!should_expand)
+		return (1);
+	if (!astr_create(&a))
+		return (0);
+
+	if (!)
+
+	return (1);
 }
 
 int do_prompt(t_app *app, char const *heredoc_end)
 {
-	// TODO: call try_open_heredoc() etc. etc.
+	int	fd;
+
+	if (try_open_heredoc(app, &fd) && get_heredoc_input(app, &fd, heredoc_end,
+		exp))
+	{
+	}
 	return (0);
 }
 
-//
-//	Prompt the user for all the here documents inside the tokens.
-//
 int	prompt_heredoc(t_app *app)
 {
 	unsigned int	i;
