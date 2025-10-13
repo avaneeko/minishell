47eb3ecf21bda04bedfd3912ab068e632e35adeb
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-10-13 20:01:12 +0300

    fix: Norm heredoc_bad_good_heredoc.c Change is_bad_heredoc to not cause fatal exit.

--- src/heredoc_bad_good_heredoc.c
+++ src/heredoc_bad_good_heredoc.c
@@ -1,28 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_bad_good_heredoc.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 16:30:44 by losypenk          #+#    #+#             */
+/*   Updated: 2025/10/13 20:00:07 by losypenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // This file declares:
 int	is_bad_heredoc(t_app *app, unsigned int i);
 int	is_good_heredoc(t_app *app, unsigned int i);
 
+// This function used to check if the heredoc at index i is malformed,
+// but now it's result is unused, as the syntax checker handles
+// heredoc syntax errors.
 int	is_bad_heredoc(t_app *app, unsigned int i)
 {
 	if (app->token_list->tok[i]->type == TOKEN_HEREDOC
 		&& i + 1 < app->token_list->len
 		&& app->token_list->tok[i + 1]->type != TOKEN_WORD)
 	{
-		// Heredoc followed by non-word.
-		// TODO: non-fatal.
-		write(2, "FATAL: Heredoc followed by non-word.\r\n", sizeof "FATAL: Heredoc followed by non-word.\r\n");
-		_exit(191);
 		return (1);
 	}
 	else if (app->token_list->tok[i]->type == TOKEN_HEREDOC
 		&& i + 1 >= app->token_list->len)
 	{
-		// Heredoc hanging at the end of the list.
-		// TODO: non-fatal.
-		write(2, "FATAL: Heredoc hanging at the end of the list.\r\n", sizeof "FATAL: Heredoc hanging at the end of the list.\r\n" - 1);
-		_exit(191);
 		return (1);
 	}
 	return (0);
@@ -31,6 +38,6 @@ int	is_bad_heredoc(t_app *app, unsigned int i)
 int	is_good_heredoc(t_app *app, unsigned int i)
 {
 	return (app->token_list->tok[i]->type == TOKEN_HEREDOC
-			&& i + 1 < app->token_list->len
-			&& app->token_list->tok[i + 1]->type == TOKEN_WORD);
+		&& i + 1 < app->token_list->len
+		&& app->token_list->tok[i + 1]->type == TOKEN_WORD);
 }
