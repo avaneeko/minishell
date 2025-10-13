942ba7342d90c5acde1baa206c4339ac67863bb2
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-10-13 17:14:39 +0300

    fix: vg.sh

--- vg.sh
+++ vg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-make CF='-glldb' CC=clang -Wno-error -j12 &&
+make CF='-glldb -Wno-error' CC=clang -j12 &&
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes \
 	--track-fds=yes -s --suppressions=vg.supp ./minishell
