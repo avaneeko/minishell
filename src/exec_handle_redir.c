0335e24d1f2b81674d13913eba23b9ec07f9c27c
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-10-13 14:19:36 +0300

    fix: Norm exec_handle_redir.c

--- src/exec_handle_redir.c
+++ src/exec_handle_redir.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_handle_redir.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 14:19:07 by losypenk          #+#    #+#             */
+/*   Updated: 2025/10/13 14:19:25 by losypenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 //$ Purpose: Handles a redirection token, adding it to the command's
@@ -10,7 +22,7 @@ int	handle_redirect_token(t_command *cmd, t_token *t, char *redir_str,
 int	handle_redirect_token(t_command *cmd, t_token *t, char *redir_str,
 		int unsigned *cur_redir)
 {
-	t_redir *redir;
+	t_redir	*redir;
 
 	redir = malloc(sizeof(t_redir));
 	if (!redir)
@@ -18,9 +30,9 @@ int	handle_redirect_token(t_command *cmd, t_token *t, char *redir_str,
 	redir->next = 0;
 	redir->type = t->type;
 	if (redir->type == TOKEN_HEREDOC)
-		redir->target = (char *)(uintptr_t)t->token[0]; // Has the heredoc fd in ptr.
+		redir->target = (char *)(uintptr_t)t->token[0];
 	else
-		redir->target = redir_str; // Filepath
+		redir->target = redir_str;
 	cmd->redirs[(*cur_redir)++] = redir;
 	return (1);
 }
