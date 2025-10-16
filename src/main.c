765133b4886582a53efa2fac32c14a3d16c294f6
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-10-16 16:46:59 +0300

    refactor: main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 16:38:05 by losypenk          #+#    #+#             */
+/*   Updated: 2025/10/16 16:46:27 by losypenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // From signals.c
 void	set_default_signals(void);
 void	set_heredoc_signals(void);
 
-t_app *get_app(void)
+t_app	*get_app(void)
 {
-	static t_app app;
+	static t_app	app;
+
 	return (&app);
 }
 
-static void reset(t_app * app)
+static void	reset(t_app *app)
 {
 	clear_token_list(app->token_list);
 	app_reset_heredocs(app);
@@ -22,36 +35,42 @@ static void reset(t_app * app)
 	app->skip_exec = 0;
 }
 
+static void	exec(t_app *app)
+{
+	dequote_tokens(app);
+	if (is_syntax_valid(app) && !app->skip_exec)
+	{
+		build_exec(app);
+		if (app->exec.len)
+			app->last_exit_code = execute_pipeline(app, &app->exec.cmds[0],
+					&app->env);
+		else
+			app->last_exit_code = execute_pipeline(app, NULL, &app->env);
+	}
+}
+
 int	main(int argc, char const *argv[], char const *envp[])
 {
-	t_app *app = get_app();
+	t_app *const	app = get_app();
 
 	if (!app_create(argc, argv, envp, app))
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	while (prompt(app) > 0)
 	{
 		if (!tokenize(app->prompt, app->token_list))
 		{
 			write(1, "Tokenizer error.\n", sizeof "Tokenizer error.\n" - 1);
+			clear_token_list(app->token_list);
 			free(app->prompt);
-			continue;
+			continue ;
 		}
-		if (!prompt_heredoc(app))
+		if (!prompt_heredoc(app) && !expand(app, &app->token_list, &app->env)
+			&& !token_resplit(app))
 		{
 			reset(app);
 			continue ;
 		}
-		expand(app, &app->token_list, &app->env);
-		token_resplit(app);
-		dequote_tokens(app);
-		if (is_syntax_valid(app) && !app->skip_exec)
-		{
-			build_exec(app);
-			if (app->exec.len)
-				app->last_exit_code = execute_pipeline(app, &app->exec.cmds[0], &app->env);
-			else
-				app->last_exit_code = execute_pipeline(app, NULL, &app->env);
-		}
+		exec(app);
 		reset(app);
 	}
 	app_destroy(app);
