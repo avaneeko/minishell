88db1c0178d839228c331d6698e9251f41b83b06
avaneeko <222704844+avaneeko@users.noreply.github.com>
2025-10-16 17:45:46 +0300

    Remove misc files.

--- vg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-make CF='-glldb -Wno-error' CC=clang -j12 &&
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes \
-	--track-fds=yes -s --suppressions=vg.supp ./minishell
